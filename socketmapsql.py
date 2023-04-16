f6a6634feab7ec880cbd9850bde5c498757ee30d
Keith Gaughan <k@stereochro.me>
2023-04-16 18:43:21 +0100

    Modernise and add type annotations

--- socketmapsql.py
+++ socketmapsql.py
@@ -5,43 +5,57 @@ to be queried out of process.
 """
 
 import argparse
-import contextlib
 import configparser
+import contextlib
+import dataclasses
 import importlib
+import io
 import os.path
 import re
 import select
 import subprocess
 import sys
+from typing import Callable, Dict, List, Mapping, Optional, Tuple, TypedDict
 
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
+
+
+Transform = Callable[[str, Mapping[str, str]], List[str]]
+
+
+@dataclasses.dataclass
+class TableCfg:
+    query: str
+    transform: Transform
+
+
+class Cfg(TypedDict):
+    database: Dict[str, str]
+    misc: Dict[str, str]
+    tables: Dict[str, TableCfg]
 
 
 FUNC_REF_PATTERN = re.compile(
     r"""
     ^
-    (?P<module>
-        [a-z_][a-z0-9_]*(?:\.[a-z_][a-z0-9_]*)*
-    )
+    (?P<module>[a-z_][a-z0-9_]*(?:\.[a-z_][a-z0-9_]*)*)
     :
-    (?P<object>
-        [a-z_][a-z0-9_]*(?:\.[a-z_][a-z0-9_]*)*
-    )
+    (?P<object>[a-z_][a-z0-9_]*(?:\.[a-z_][a-z0-9_]*)*)
     $
     """,
     re.I | re.X,
 )
 
 
-def match(name):
-    if matches := FUNC_REF_PATTERN.match(name):
-        return matches.group("module"), matches.group("object")
-    else:
+def match(name: str) -> Tuple[str, str]:
+    matches = FUNC_REF_PATTERN.match(name)
+    if not matches:
         raise ValueError(f"Malformed callable '{name}'")
+    return matches.group("module"), matches.group("object")
 
 
-def resolve(module_name, obj_name):
+def resolve(module_name: str, obj_name: str) -> Callable:
     """
     Resolve a named object in a module.
     """
@@ -52,30 +66,32 @@ class MalformedNetstringError(Exception):
     pass
 
 
-def read_netstring(fp):
+def read_netstring(fp: io.TextIOWrapper) -> Optional[str]:
     """
     Reads a single netstring.
     """
-    n = ""
+    ns = ""
     while True:
         c = fp.read(1)
         if c == "":
             return None
         if c == ":":
             break
-        if len(n) > 10:
+        if len(ns) > 10:
             raise MalformedNetstringError
-        if c == "0" and n == "":
+        if c == "0" and ns == "":
             # We can't allow leading zeros.
             if fp.read(1) != ":":
                 raise MalformedNetstringError
-            n = c
+            ns = c
             break
-        n += c
-    n = int(n, 10)
+        ns += c
+    n = int(ns, 10)
     result = ""
     while n > 0:
         segment = fp.read(n)
+        if segment == "":
+            raise MalformedNetstringError
         n -= len(segment)
         result += segment
     if fp.read(1) != ",":
@@ -83,24 +99,26 @@ def read_netstring(fp):
     return result
 
 
-def write_netstring(fp, response):
+def write_netstring(fp: io.TextIOWrapper, response: str):
     fp.write(f"{len(response)}:{response},")
     fp.flush()
 
 
-def parse_config(fp):
-    def process_local(local_part, cfg):
-        delimiter = cfg.get("recipient_delimiter", "").strip()
-        if delimiter != "":
-            local_part = local_part.split(delimiter, 1)[0]
-        return local_part
+def process_local(local_part: str, cfg: Mapping[str, str]) -> str:
+    delimiter = cfg.get("recipient_delimiter", "").strip()
+    if delimiter != "":
+        local_part = local_part.split(delimiter, 1)[0]
+    return local_part.lower()
 
-    def split(arg, cfg):
-        parts = arg.split("@", 1)
-        parts[0] = process_local(parts[0], cfg)
-        parts[1] = parts[1].lower()
-        return parts
 
+def split(arg: str, cfg: Mapping[str, str]) -> List[str]:
+    parts = arg.split("@", 1)
+    parts[0] = process_local(parts[0], cfg)
+    parts[1] = parts[1].lower()
+    return parts
+
+
+def parse_config(fp: io.TextIOWrapper) -> Cfg:
     transforms = {
         "all": lambda arg, cfg: [arg],
         "lowercase": lambda arg, cfg: [arg.lower()],
@@ -109,15 +127,10 @@ def parse_config(fp):
         "split": split,
     }
 
-    cp = configparser.RawConfigParser()
-    cp.readfp(fp)
+    cp = configparser.ConfigParser(interpolation=None)
+    cp.read_file(fp)
 
-    result = {
-        "database": dict(cp.items("database")),
-        "tables": {},
-        "misc": dict(cp.items("misc")),
-    }
-
+    tables = {}
     for section in cp.sections():
         if section.startswith("table:"):
             _, table_name = section.split(":", 1)
@@ -132,34 +145,59 @@ def parse_config(fp):
             if transform is None:
                 transform = resolve(*match(transform_name))
 
-            result["tables"][table_name] = {
-                "transform": transform,
-                "query": cp.get(section, "query"),
-            }
+            tables[table_name] = TableCfg(
+                query=cp.get(section, "query"),
+                transform=transform,
+            )
 
-    return result
+    return Cfg(
+        database=dict(cp.items("database")),
+        tables=tables,
+        misc=dict(cp.items("misc")),
+    )
 
 
-def serve_client(fh_in, fh_out, conn, timeout, tables, cfg):
+def get_int(cfg: Mapping[str, str], key: str) -> Optional[int]:
+    if key in cfg:
+        return int(cfg[key])
+    return None
+
+
+def serve_client(
+    fh_in: io.TextIOWrapper,
+    fh_out: io.TextIOWrapper,
+    conn,
+    timeout: int,
+    tables: Mapping[str, TableCfg],
+    cfg: Mapping[str, str],
+):
+    max_requests = get_int(cfg, "max_requests")
     try:
         while True:
+            if max_requests is not None:
+                max_requests -= 1
+                if max_requests < 1:
+                    break
+
             # Wait a short period before exiting.
             iready, _, _ = select.select([fh_in], (), (), timeout)
             if len(iready) == 0:
                 break
 
             request = read_netstring(fh_in)
+            if request is None:
+                break
 
             table_name, arg = request.split(" ", 1)
 
             table = tables.get(table_name)
             if table is None:
                 write_netstring(fh_out, f"PERM no such table: {table_name}")
-                continue
+                break
 
             cur = conn.cursor()
             try:
-                cur.execute(table["query"], table["transform"](arg, cfg))
+                cur.execute(table.query, table.transform(arg, cfg))
                 result = cur.fetchone()
             finally:
                 cur.close()
@@ -174,7 +212,7 @@ def serve_client(fh_in, fh_out, conn, timeout, tables, cfg):
         write_netstring(fh_out, f"PERM {str(exc)}")
 
 
-def connect(settings):
+def connect(settings: Dict[str, str]):
     """
     Connect to a database.
     """
@@ -182,7 +220,7 @@ def connect(settings):
     return driver.connect(**settings)
 
 
-def main():
+def make_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="Database socketmap daemon.")
     parser.add_argument(
         "--config",
@@ -194,12 +232,18 @@ def main():
         "--timeout",
         help="Number of seconds to wait before exiting",
         type=int,
-        default=2,
+        default=5,
     )
     parser.add_argument(
-        "--client", action="store_true", help="Start with a debug client"
+        "--client",
+        action="store_true",
+        help="Start with a debug client",
     )
-    args = parser.parse_args()
+    return parser
+
+
+def main():
+    args = make_parser().parse_args()
 
     with contextlib.closing(args.config):
         cfg = parse_config(args.config)
@@ -208,7 +252,10 @@ def main():
         svr_args = [arg for arg in sys.argv if arg != "--client"]
         svr_args[0] = os.path.abspath(svr_args[0])
         proc = subprocess.Popen(
-            svr_args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding="utf-8"
+            svr_args,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
         )
         print("Type '.exit' to exit.")
         while proc.poll() is None:
@@ -221,7 +268,12 @@ def main():
     else:
         with contextlib.closing(connect(cfg["database"])) as conn:
             serve_client(
-                sys.stdin, sys.stdout, conn, args.timeout, cfg["tables"], cfg["misc"]
+                sys.stdin,
+                sys.stdout,
+                conn,
+                args.timeout,
+                cfg["tables"],
+                cfg["misc"],
             )
 
     return 0
