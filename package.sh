6115b335b131d1827d688dced481be3583843a64
Keith Gaughan <k@stereochro.me>
2026-06-25 21:06:46 +0100

    Config packaging with nfpm
    This is the final configuration and scripts from this blogpost:
    
    https://keith.gaughan.ie/creating-a-signed-rpm-with-nfpm-and-gnupg.html

---
+++ package.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set -e
+
+usage () {
+	cat <<FIN
+Usage:
+  $0 [-P]
+  $0 -h
+
+Flags:
+  -h  show this help
+  -P  prompt for a passphrase; setting NFPM_PASSPHRASE makes this a no-op
+
+FIN
+}
+
+while getopts "hP" opt; do
+	case "$opt" in
+		h)
+			usage
+			exit 0
+			;;
+		P)
+			prompt=1
+			;;
+		*)
+			usage 2>&1
+			exit 1
+			;;
+	esac
+done
+
+if test "${prompt:-}" = "1" -a -z "${NFPM_PASSPHRASE:-}"; then
+	stty -echo
+	read -p "passphrase> " NFPM_PASSPHRASE
+	stty echo
+	echo
+	export NFPM_PASSPHRASE
+fi
+
+export VERSION=$(git describe --tags --abbrev=0 || echo "v0.0.0")
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+
+nfpm package --packager rpm
